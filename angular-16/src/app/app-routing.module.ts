3d04f88b122c8c42f65366203b1d671b7213218d
MedAmineMahid <132580581+MedAmineMahid@users.noreply.github.com>
2024-07-18 12:27:50 +0100

    first commit

---
+++ angular-16/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './dashboard/dashboard-components/product/product.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FullComponent } from './layouts/full/full.component';
+
+const routes: Routes = [
+  {
+    path:"",
+    component:FullComponent,
+    children: [
+      {path:"", redirectTo:"/home", pathMatch:"full"},
+      {path:"table", component:ProductComponent},
+      {path:"home", component:DashboardComponent},
+    ]
+  },
+
+  {path:"", redirectTo:"/home", pathMatch:"full"},
+  {path:"**", redirectTo:"/home", pathMatch:"full"},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
