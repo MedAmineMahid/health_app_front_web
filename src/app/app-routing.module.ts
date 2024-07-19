950dc2dcdcc912f4b589de9c834307a5050e599b
soumia <soumiaoukhira66@gmail.com>
2024-07-20 00:15:13 +0100

    welcome page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './dashboard/dashboard-components/product/product.component';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import { FullComponent } from './layouts/full/full.component';
+import {HomeComponent} from "./home/home.component";
+import {SignupComponent} from "./auth/signup/signup.component";
+
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"signUp",component:SignupComponent},
+  {
+    path:"",
+    component:FullComponent,
+    children: [
+      {path:"table", component:ProductComponent},
+      {path:"home", component:DashboardComponent},
+    ]
+  },
+
+  {path:"", redirectTo:"/home", pathMatch:"full"},
+  {path:"**", redirectTo:"/home", pathMatch:"full"},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
