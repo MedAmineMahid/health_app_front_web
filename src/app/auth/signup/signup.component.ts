a6f478411266d2d1ca0f400589472b19e0d9d23d
soumia <soumiaoukhira66@gmail.com>
2024-07-20 18:08:55 +0100

    auth forms

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+constructor() {}
 
+  ngOnInit(): void {
+    const sign_in_btn = document.querySelector("#sign-in-btn") as HTMLElement;
+    const sign_up_btn = document.querySelector("#sign-up-btn") as HTMLElement;
+    const container = document.querySelector(".container") as HTMLElement;
+
+    sign_up_btn.addEventListener("click", () => {
+      container.classList.add("sign-up-mode");
+      this.animateBackground();
+    });
+
+    sign_in_btn.addEventListener("click", () => {
+      container.classList.remove("sign-up-mode");
+    });
+  }
+
+  animateBackground() {
+    const container = document.querySelector(".container") as HTMLElement;
+    const circles = 5;
+
+    for (let i = 0; i < circles; i++) {
+      const circle = document.createElement('div');
+      circle.classList.add('animated-circle');
+      circle.style.setProperty('--delay', `${i * 0.2}s`);
+      container.appendChild(circle);
+
+      setTimeout(() => {
+        circle.remove();
+      }, 2000);
+    }
+  }
 }
